498fa7ff3c4ea226a42928061471e7034c971532
AlexRansom6405 <143015774+AlexRansom6405@users.noreply.github.com>
2023-11-03 14:35:32 -0400

    Add files via upload

--- player.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-@export var speed = 400 # How fast the playeer will move (pixels/sec).
-var screen_size # Size of the game window. 
-
-func _ready():
-	screen_size = get_viewport_rect().size
